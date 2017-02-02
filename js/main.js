2bd788c59259758072b0e0b6cd94ab8e3e8e7096
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2017-02-03 10:02:06 +1100

    add new sections and animations with wow.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+
+// wow = new WOW({
+//   boxClass:       'wow',
+//   animationClass: 'animated',
+//   offset:         0,
+//   mobile:         true,
+//   live:           true,
+// })wow.init();
