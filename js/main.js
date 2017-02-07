a0c5f4212d982e24ddfbb876b234e67b59539a94
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2017-02-07 13:39:33 +1100

    Add background images and final styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 new WOW().init();
 
+console.log("%cHi there, thanks for looking further into my work.. give me a call and lets chat!",
+"background-color: gainsboro;font-size: 15px; color: dodgerblue;");
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 // wow = new WOW({
 //   boxClass:       'wow',
 //   animationClass: 'animated',
