701bf5d9bade25cd830ae2b6ff02269c3d06bf0e
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2017-02-20 15:14:37 +1100

    Fixed stupid spelling mistakes

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
 new WOW().init();
 
 console.log("%cHi there, thanks for looking further into my work.. give me a call and lets chat!",
-"background-color: gainsboro;font-size: 15px; color: dodgerblue;");
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+    "background-color: gainsboro;font-size: 15px; color: dodgerblue;");
 
-// wow = new WOW({
-//   boxClass:       'wow',
-//   animationClass: 'animated',
-//   offset:         0,
-//   mobile:         true,
-//   live:           true,
-// })wow.init();
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
